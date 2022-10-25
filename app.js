b7a7d2297b66f3fa1c3800d76aaa6d4cf39a5d45
Evgeniy Mihievich <rashmibalajirashmi@gmail.com>
2022-10-25 17:35:55 +0300

    добавил celebrate

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const { celebrate } = require('celebrate');
 const express = require('express');
 const mongoose = require('mongoose');
+const { Joi } = require('celebrate');
 
 const { createUser, login } = require('./controllers/users');
 
@@ -13,14 +15,27 @@ app.disable('x-powered-by');
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(7),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(7),
+    name: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    about: Joi.string().min(2).max(30),
+  }),
+}), createUser);
 
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'Не найдено' });
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   const { statusCode = 500, message } = err;
 
   res
@@ -28,7 +43,7 @@ app.use((err, req, res, next) => {
     .send({
       message: statusCode === 500
         ? 'На сервере произошла ошибка'
-        : message
+        : message,
     });
 });
 
