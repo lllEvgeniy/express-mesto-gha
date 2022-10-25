01facd14d57d95080e183aabefa0b756ad93d094
Evgeniy Mihievich <rashmibalajirashmi@gmail.com>
2022-10-25 18:52:05 +0300

    добавил celebrate

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth');
 const {
   getUser, getUserById, updateAvatar, updateUser, currentUser,
@@ -6,11 +7,25 @@ const {
 
 router.use(auth);
 router.get('/', getUser);
-
 router.get('/me', currentUser);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
 
-router.get('/:userId', getUserById);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUser);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required(),
+  }),
+}), updateAvatar);
+
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().required().length(24),
+  }),
+}), getUserById);
 
 module.exports = router;
